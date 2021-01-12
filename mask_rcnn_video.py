51a82f791483cd624c92084c8fc6a62c2337d94d
Anzurna <aversatrix@mail.ru>
2021-01-12 16:02:18 +0300

    Minor fixes

--- mask_rcnn_video.py
+++ mask_rcnn_video.py
@@ -1,7 +1,6 @@
 # USAGE
 # python mask_rcnn_video.py --input videos/cats_and_dogs.mp4 --output output/cats_and_dogs_output.avi --mask-rcnn mask-rcnn-coco
 
-# import the necessary packages
 import numpy as np
 import argparse
 import imutils
@@ -44,26 +43,19 @@ configPath = os.path.sep.join([args["mask_rcnn"],
 print("[INFO] loading Mask R-CNN from disk...")
 net = cv2.dnn.readNetFromTensorflow(weightsPath, configPath)
 
-# initialize the video stream and pointer to output video file
 vs = cv2.VideoCapture(args["input"])
 writer = None
-
-# try to determine the total number of frames in the video file
+e
 try:
 	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
 		else cv2.CAP_PROP_FRAME_COUNT
 	total = int(vs.get(prop))
 	print("[INFO] {} total frames in video".format(total))
-
-# an error occurred while trying to determine the total
-# number of frames in the video file
 except:
 	print("[INFO] could not determine # of frames in video")
 	total = -1
 
-# loop over frames from the video file stream
 while True:
-	# read the next frame from the file
 	(grabbed, frame) = vs.read()
 
 	# if the frame was not grabbed, then we have reached the end
@@ -82,11 +74,7 @@ while True:
 		"detection_masks"])
 	end = time.time()
 
-	# loop over the number of detected objects
 	for i in range(0, boxes.shape[2]):
-		# extract the class ID of the detection along with the
-		# confidence (i.e., probability) associated with the
-		# prediction
 		classID = int(boxes[0, 0, i, 1])
 		confidence = boxes[0, 0, i, 2]
 
@@ -102,57 +90,39 @@ while True:
 			boxW = endX - startX
 			boxH = endY - startY
 
-			# extract the pixel-wise segmentation for the object,
-			# resize the mask such that it's the same dimensions of
-			# the bounding box, and then finally threshold to create
-			# a *binary* mask
 			mask = masks[i, classID]
 			mask = cv2.resize(mask, (boxW, boxH),
 				interpolation=cv2.INTER_NEAREST)
 			mask = (mask > args["threshold"])
 
-			# extract the ROI of the image but *only* extracted the
-			# masked region of the ROI
 			roi = frame[startY:endY, startX:endX][mask]
 
-			# grab the color used to visualize this particular class,
-			# then create a transparent overlay by blending the color
-			# with the ROI
 			color = COLORS[classID]
 			blended = ((0.4 * color) + (0.6 * roi)).astype("uint8")
 
-			# store the blended ROI in the original frame
 			frame[startY:endY, startX:endX][mask] = blended
 
-			# draw the bounding box of the instance on the frame
 			color = [int(c) for c in color]
 			cv2.rectangle(frame, (startX, startY), (endX, endY),
 				color, 2)
 
-			# draw the predicted label and associated probability of
-			# the instance segmentation on the frame
 			text = "{}: {:.4f}".format(LABELS[classID], confidence)
 			cv2.putText(frame, text, (startX, startY - 5),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-	# check if the video writer is None
 	if writer is None:
-		# initialize our video writer
 		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
 		writer = cv2.VideoWriter(args["output"], fourcc, 30,
 			(frame.shape[1], frame.shape[0]), True)
 
-		# some information on processing single frame
 		if total > 0:
 			elap = (end - start)
 			print("[INFO] single frame took {:.4f} seconds".format(elap))
 			print("[INFO] estimated total time to finish: {:.4f}".format(
 				elap * total))
 
-	# write the output frame to disk
 	writer.write(frame)
 
-# release the file pointers
 print("[INFO] cleaning up...")
 writer.release()
 vs.release()
